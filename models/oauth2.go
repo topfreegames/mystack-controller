403b253a922f7809b0402fc3155dfb9a167cb052
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-03-28 09:34:22 -0300

    Functions moved to kubecos-cli

--- models/oauth2.go
+++
@@ -1,8 +0,0 @@
-// kubecos api
-// https://github.com/topfreegames/kubecos
-//
-// Licensed under the MIT license:
-// http://www.opensource.org/licenses/mit-license
-// Copyright © 2017 Top Free Games <backend@tfgco.com>
-
-package models
