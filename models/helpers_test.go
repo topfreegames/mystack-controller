f6c20b212b636770043d6509e9125fee10584897
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-03-28 13:43:07 -0300

    GetDB test

---
+++ models/helpers_test.go
@@ -0,0 +1,48 @@
+// mystack-controller api
+// https://github.com/topfreegames/mystack-controller
+//
+// Licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license
+// Copyright © 2017 Top Free Games <backend@tfgco.com>
+
+package models_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/topfreegames/kubecos/kubecos-controller/models"
+)
+
+var _ = Describe("Helpers", func() {
+	Describe("GetDB", func() {
+		It("should return a DB connection successfully", func() {
+			db, err := models.GetDB(
+				"localhost", "mystack_controller_test", 8585, "disable",
+				"mystack_controller_test", "",
+				10, 10, 100,
+			)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(db).NotTo(BeNil())
+		})
+
+		It("should succeed if connectionTimeoutMS value is <= 0", func() {
+			db, err := models.GetDB(
+				"localhost", "mystack_controller_test", 8585, "disable",
+				"mystack_controller_test", "",
+				10, 10, -50,
+			)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(db).NotTo(BeNil())
+		})
+
+		It("should panic if invalid connection information is passed", func() {
+			Expect(func() {
+				models.GetDB(
+					"localhost", "mystack_controller_testtt", 8585, "disable",
+					"mystack_controller_test", "password",
+					10, 10, 100,
+				)
+			}).To(Panic())
+		})
+	})
+})
