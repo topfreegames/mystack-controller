116fa503550e30424deb992eb53e66a326316721
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-03-29 10:34:33 -0300

    Get URL test

---
+++ extensions/login_test.go
@@ -0,0 +1,19 @@
+package extensions_test
+
+import (
+	. "github.com/topfreegames/mystack/mystack-controller/extensions"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Login", func() {
+	Describe("Generate Login URL", func() {
+		It("should return an valid URL", func() {
+			state := "random"
+			url, err := GenerateLoginURL(state)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(url).To(ContainSubstring(state))
+		})
+	})
+})
