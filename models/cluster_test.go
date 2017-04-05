23d041538e288dc33d706e9ed499c40818b43285
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-04-05 14:15:39 -0300

    Delete cluster tests

--- models/cluster_test.go
+++ models/cluster_test.go
@@ -29,6 +29,11 @@ var _ = Describe("Cluster", func() {
 			LabelSelector: labelMap.AsSelector().String(),
 			FieldSelector: fields.Everything().String(),
 		}
+		deployments = []*Deployment{
+			NewDeployment("app1", username, "image1", port),
+			NewDeployment("app2", username, "image2", port),
+			NewDeployment("app3", username, "image3", port),
+		}
 	)
 
 	BeforeEach(func() {
@@ -37,12 +42,6 @@ var _ = Describe("Cluster", func() {
 
 	Describe("Create", func() {
 		It("should create cluster", func() {
-			deployments := []*Deployment{
-				NewDeployment("app1", username, "image1", port),
-				NewDeployment("app2", username, "image2", port),
-				NewDeployment("app3", username, "image3", port),
-			}
-
 			cluster := NewCluster(username, deployments)
 			err := cluster.Create(clientset)
 			Expect(err).NotTo(HaveOccurred())
@@ -55,9 +54,77 @@ var _ = Describe("Cluster", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(services.Items).To(HaveLen(3))
 		})
+
+		It("should return error if creating same cluster twice", func() {
+			cluster := NewCluster(username, deployments)
+			err := cluster.Create(clientset)
+			Expect(err).NotTo(HaveOccurred())
+
+			err = cluster.Create(clientset)
+			Expect(err).To(HaveOccurred())
+		})
 	})
 
 	Describe("Delete", func() {
+		It("should delete cluster", func() {
+			cluster := NewCluster(username, deployments)
+			err := cluster.Create(clientset)
+			Expect(err).NotTo(HaveOccurred())
 
+			err = cluster.Delete(clientset)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(NamespaceExists(clientset, namespace)).To(BeFalse())
+
+			deploys, err := clientset.ExtensionsV1beta1().Deployments(namespace).List(listOptions)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(deploys.Items).To(BeEmpty())
+
+			services, err := clientset.CoreV1().Services(namespace).List(listOptions)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(services.Items).To(BeEmpty())
+		})
+
+		It("should delete only specified cluster", func() {
+			deployments1 := []*Deployment{
+				NewDeployment("app1", "user1", "image1", port),
+				NewDeployment("app2", "user1", "image2", port),
+				NewDeployment("app3", "user1", "image3", port),
+			}
+			cluster1 := NewCluster("user1", deployments1)
+			err := cluster1.Create(clientset)
+			Expect(err).NotTo(HaveOccurred())
+
+			deployments2 := []*Deployment{
+				NewDeployment("app1", "user2", "image1", port),
+				NewDeployment("app2", "user2", "image2", port),
+				NewDeployment("app3", "user2", "image3", port),
+			}
+			cluster2 := NewCluster("user2", deployments2)
+			err = cluster2.Create(clientset)
+			Expect(err).NotTo(HaveOccurred())
+
+			err = cluster1.Delete(clientset)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(NamespaceExists(clientset, "mystack-user1")).To(BeFalse())
+			Expect(NamespaceExists(clientset, "mystack-user2")).To(BeTrue())
+
+			deploys, err := clientset.ExtensionsV1beta1().Deployments("mystack-user1").List(listOptions)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(deploys.Items).To(BeEmpty())
+
+			services, err := clientset.CoreV1().Services("mystack-user1").List(listOptions)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(services.Items).To(BeEmpty())
+
+			deploys, err = clientset.ExtensionsV1beta1().Deployments("mystack-user2").List(listOptions)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(deploys.Items).To(HaveLen(3))
+
+			services, err = clientset.CoreV1().Services("mystack-user2").List(listOptions)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(services.Items).To(HaveLen(3))
+		})
 	})
 })
