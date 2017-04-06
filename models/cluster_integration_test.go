1d28518f47ebe86ad24109144233c12fe99a3a2b
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-04-06 19:10:30 -0300

    NewCluster unit test

---
+++ models/cluster_integration_test.go
@@ -0,0 +1,18 @@
+// mystack-controller api
+// +build integration
+// https://github.com/topfreegames/mystack-controller
+//
+// Licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license
+// Copyright © 2017 Top Free Games <backend@tfgco.com>
+
+package models_test
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Cluster", func() {
+	Describe("NewCluster", func() {
+	})
+})
--- models/cluster_test.go
+++ models/cluster_test.go
@@ -13,44 +13,113 @@ import (
 	. "github.com/onsi/gomega"
 	. "github.com/topfreegames/mystack-controller/models"
 
+	"database/sql"
+	"github.com/jmoiron/sqlx"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/pkg/fields"
 	"k8s.io/client-go/pkg/labels"
 )
 
-var (
-	clientset   *fake.Clientset
-	username    = "user"
-	namespace   = "mystack-user"
-	port        = 5000
-	labelMap    = labels.Set{"mystack/routable": "true"}
-	listOptions = v1.ListOptions{
-		LabelSelector: labelMap.AsSelector().String(),
-		FieldSelector: fields.Everything().String(),
-	}
-)
-
-func mockCluster(username string) *Cluster {
-	return &Cluster{
-		Username:  username,
-		Namespace: namespace,
-		Deployments: []*Deployment{
-			NewDeployment("test1", username, "app1", port, nil),
-			NewDeployment("test2", username, "app2", port, nil),
-			NewDeployment("test3", username, "app3", port, nil),
-		},
-		Services: []*Service{
-			NewService("test1", username, 80, port),
-			NewService("test2", username, 80, port),
-			NewService("test3", username, 80, port),
-		},
-	}
-}
-
 var _ = Describe("Cluster", func() {
+	const (
+		yaml1 = `
+apps:
+  test1:
+    image: app1
+    port: 5000
+  test2:
+    image: app2
+    port: 5000
+  test3:
+    image: app3
+    port: 5000
+`
+	)
+	var (
+		db          *sql.DB
+		sqlxDB      *sqlx.DB
+		mock        sqlmock.Sqlmock
+		err         error
+		clusterName = "MyCustomApps"
+		clientset   *fake.Clientset
+		username    = "user"
+		namespace   = "mystack-user"
+		port        = 5000
+		labelMap    = labels.Set{"mystack/routable": "true"}
+		listOptions = v1.ListOptions{
+			LabelSelector: labelMap.AsSelector().String(),
+			FieldSelector: fields.Everything().String(),
+		}
+	)
+
+	mockCluster := func(username string) *Cluster {
+		return &Cluster{
+			Username:  username,
+			Namespace: namespace,
+			Deployments: []*Deployment{
+				NewDeployment("test1", username, "app1", port, nil),
+				NewDeployment("test2", username, "app2", port, nil),
+				NewDeployment("test3", username, "app3", port, nil),
+			},
+			Services: []*Service{
+				NewService("test1", username, 80, port),
+				NewService("test2", username, 80, port),
+				NewService("test3", username, 80, port),
+			},
+		}
+	}
+
 	BeforeEach(func() {
 		clientset = fake.NewSimpleClientset()
+		db, mock, err = sqlmock.New()
+		Expect(err).NotTo(HaveOccurred())
+		sqlxDB = sqlx.NewDb(db, "postgres")
+	})
+
+	Describe("NewCluster", func() {
+		It("should return cluster from config on DB", func() {
+			defer db.Close()
+
+			mockedCluster := mockCluster(username)
+
+			mock.
+				ExpectExec("INSERT INTO clusters").
+				WithArgs(clusterName, yaml1).
+				WillReturnResult(sqlmock.NewResult(1, 1))
+			mock.
+				ExpectQuery("^SELECT yaml FROM clusters WHERE name = (.+)$").
+				WithArgs(clusterName).
+				WillReturnRows(sqlmock.NewRows([]string{"yaml"}).AddRow(yaml1))
+
+			err = WriteClusterConfig(sqlxDB, clusterName, yaml1)
+			Expect(err).NotTo(HaveOccurred())
+
+			cluster, err := NewCluster(sqlxDB, username, clusterName)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cluster.Deployments).To(ConsistOf(mockedCluster.Deployments))
+			Expect(cluster.Services).To(ConsistOf(mockedCluster.Services))
+
+			err = mock.ExpectationsWereMet()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should return error if clusterName doesn't exists on DB", func() {
+			defer db.Close()
+
+			mock.
+				ExpectQuery("^SELECT yaml FROM clusters WHERE name = (.+)$").
+				WithArgs(clusterName).
+				WillReturnRows(sqlmock.NewRows([]string{"yaml"}))
+
+			cluster, err := NewCluster(sqlxDB, username, clusterName)
+			Expect(cluster).To(BeNil())
+			Expect(err).To(HaveOccurred())
+
+			err = mock.ExpectationsWereMet()
+			Expect(err).NotTo(HaveOccurred())
+		})
 	})
 
 	Describe("Create", func() {
