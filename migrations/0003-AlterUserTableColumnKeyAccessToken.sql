fac88f553f483f158da2c189005e5f213995e004
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2017-05-29 16:51:53 -0300

    update migration

--- migrations/0003-AlterUserTableColumnKeyAccessToken.sql
+++ migrations/0003-AlterUserTableColumnKeyAccessToken.sql
@@ -5,9 +5,7 @@
 -- http://www.opensource.org/licenses/mit-license
 -- Copyright © 2016 Top Free Games <backend@tfgco.com>
 
-ALTER TABLE users 
-  ADD COLUMN key_access_token varchar(255) UNIQUE NOT NULL CHECK (key_access_token <> '') DEFAULT 'token';
-
+ALTER TABLE users ADD COLUMN key_access_token varchar(255) NOT NULL CHECK (key_access_token <> '') DEFAULT 'token';
 UPDATE users SET key_access_token = access_token;
-
 ALTER TABLE users ALTER COLUMN key_access_token DROP DEFAULT;
+CREATE UNIQUE INDEX ON users (key_access_token);
